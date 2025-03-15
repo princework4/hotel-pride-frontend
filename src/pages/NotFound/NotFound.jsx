2b3b35803c284e89e724e10ea85a84d9d97bb313
PrinceP Vishwakarma <prince.10723612@ltimindtree.com>
2025-03-15 18:58:00 +0530

    added page not found

---
+++ src/pages/NotFound/NotFound.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./NotFound.css";
+
+const NotFound = () => {
+  return (
+    <div className="not_found_container">
+      <div className="not_found_error">
+        <h1>Oops</h1>
+        <p>The Page you're looking for isn't here.</p>
+        <p className="goback_home">
+          Go back to <Link to="/">Home</Link>
+        </p>
+      </div>
+      <div className="not_found_design_1"></div>
+      <div className="not_found_design_2"></div>
+    </div>
+  );
+};
+
+export default NotFound;
