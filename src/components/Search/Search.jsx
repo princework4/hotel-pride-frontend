c92b5012a1943e3c895d27349cd6d2a3610f595d
PrinceP Vishwakarma <prince.10723612@ltimindtree.com>
2025-03-15 23:55:32 +0530

    enhanced search bar UI

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./Search.css";
 import * as React from "react";
+import { Button } from "@mui/material";
 // import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
 // import { LocalizationProvider } from '@mui/x-date-pickers-pro/LocalizationProvider';
 // import { AdapterDayjs } from '@mui/x-date-pickers-pro/AdapterDayjs';
@@ -23,8 +24,16 @@ const Search = () => {
             </div> */}
 
         <div className="form-group">
-          <label>Check-In Check Out Date</label>
-          <input type="date" />
+          <div className="date_range">
+            <div className="checkIn_date_range__container">
+              <label>Check-In</label>
+              <input type="date" />
+            </div>
+            <div className="checkOut_date_range__container">
+              <label>Check Out</label>
+              <input type="date" />
+            </div>
+          </div>
           {/* <LocalizationProvider dateAdapter={AdapterDayjs}>
                     <DemoContainer components={['DateRangePicker']}>
                        
@@ -35,45 +44,47 @@ const Search = () => {
 
         {/* Guests and Rooms */}
         <div className="form-group">
-          <label>Guests and Rooms</label>
-          <div className="guestInputs">
-            <div className="guest-group">
-              <span>Adults</span>
-              <div className="counter-buttons">
-                <button
-                  className={`sign ${adultCount == 0 ? "isDisabled" : ""}`}
-                  onClick={() => setAdultCount((c) => c - 1)}
-                >
-                  -
-                </button>
-                <input value={adultCount} />
-                <button
-                  className="sign"
-                  onClick={() => setAdultCount((c) => c + 1)}
-                >
-                  +
-                </button>
+          <div className="guest_rooms__container">
+            <label>Guests and Rooms</label>
+            <div className="guestInputs">
+              <div className="guest-group">
+                <span>Adults</span>
+                <div className="counter-buttons">
+                  <Button
+                    className={`sign ${adultCount == 0 ? "isDisabled" : ""}`}
+                    onClick={() => setAdultCount((c) => c - 1)}
+                  >
+                    -
+                  </Button>
+                  <input value={adultCount} />
+                  <Button
+                    className="sign"
+                    onClick={() => setAdultCount((c) => c + 1)}
+                  >
+                    +
+                  </Button>
+                </div>
               </div>
-            </div>
 
-            <span>|</span>
+              <span>|</span>
 
-            <div className="guest-group">
-              <span>Children</span>
-              <div className="counter-button">
-                <button
-                  className={`sign ${childCount == 0 ? "isDisabled" : ""}`}
-                  onClick={() => setChildCount((c) => c - 1)}
-                >
-                  -
-                </button>
-                <input value={childCount} />
-                <button
-                  className="sign"
-                  onClick={() => setChildCount((c) => c + 1)}
-                >
-                  +
-                </button>
+              <div className="guest-group">
+                <span>Children</span>
+                <div className="counter-button">
+                  <Button
+                    className={`sign ${childCount == 0 ? "isDisabled" : ""}`}
+                    onClick={() => setChildCount((c) => c - 1)}
+                  >
+                    -
+                  </Button>
+                  <input value={childCount} />
+                  <Button
+                    className="sign"
+                    onClick={() => setChildCount((c) => c + 1)}
+                  >
+                    +
+                  </Button>
+                </div>
               </div>
             </div>
           </div>
