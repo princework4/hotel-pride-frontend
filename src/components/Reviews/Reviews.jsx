4cdefd71bd2d26cc34265a84e18807e71eacdd3f
PrinceP Vishwakarma <prince.10723612@ltimindtree.com>
2025-03-17 14:56:38 +0530

    updated slider UI

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -11,9 +11,10 @@ const settings = {
   speed: 500,
   slidesToShow: 3,
   slidesToScroll: 1,
-  autoplay: false,
+  autoplay: true,
   autoplaySpeed: 3000,
   arrows: false,
+  adaptiveHeight: true,
   responsive: [
     {
       breakpoint: 997,
