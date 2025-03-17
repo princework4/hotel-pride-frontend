b7f69b54d33ca67ea84fbfea9850d529e3bb67ef
PrinceP Vishwakarma <prince.10723612@ltimindtree.com>
2025-03-17 14:50:15 +0530

    updated guest review slider settings

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -9,11 +9,25 @@ const settings = {
   dots: true,
   infinite: true,
   speed: 500,
-  slidesToShow: 1,
+  slidesToShow: 3,
   slidesToScroll: 1,
-  autoplay: true,
+  autoplay: false,
   autoplaySpeed: 3000,
   arrows: false,
+  responsive: [
+    {
+      breakpoint: 997,
+      settings: {
+        slidesToShow: 2,
+      },
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+      },
+    },
+  ],
 };
 
 const Reviews = ({ guestsReviews }) => {
