024055a4ac461ef4b951d8dab4019a2de03a0657
Ravinaparve <parveravina@gmail.com>
2025-03-26 21:43:27 +0530

    modified search-UI and added modal on book-now

--- src/pages/Rooms/Rooms.jsx
+++ src/pages/Rooms/Rooms.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import CustomStepper from "../../components/Stepper/Stepper";
+import Search from "../../components/Search";
 
 const Rooms = () => {
   return (
     <section>
       <div className="wrapper">
+        <Search />
         <CustomStepper />
       </div>
     </section>
