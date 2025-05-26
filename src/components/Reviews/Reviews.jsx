c47a9b881e564c9ad956d948a94c5c000be006b7
princework4 <princework4@gmail.com>
2025-05-26 10:23:26 +0530

    added modal for reviews

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Avatar, Rating } from "@mui/material";
+import { Avatar, Box, Modal, Rating } from "@mui/material";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -32,25 +32,91 @@ const settings = {
   ],
 };
 
-const Reviews = ({ guestsReviews }) => {
+const style = {
+  width: {
+    xs: "330px",
+    sm: "500px",
+  },
+  height: "auto",
+  padding: "0",
+  border: "none",
+  borderRadius: "20px",
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  bgcolor: "background.paper",
+  boxShadow: 24,
+  overflow: "hidden",
+  transform: "translate(-50%, -50%)",
+};
+
+const ReviewsContent = ({ review, modalView = true }) => {
   return (
-    <Slider {...settings} className="reviews">
-      {guestsReviews.map((review, index) => (
-        <div key={index} className="review-card">
-          <div className="review-content">
-            <Avatar
-              alt={review.name}
-              src={review.avatar}
-              sx={{ width: 56, height: 56 }}
-              className="review-avatar"
-            />
-            <h3 className="review-name">{review.name}</h3>
-            <Rating value={review.rating} readOnly className="review-rating" />
-            <p className="review-text">{review.text}</p>
+    <div
+      className="review-content"
+      style={
+        modalView
+          ? { padding: "10px 20px", margin: "0 auto" }
+          : { padding: "20px", margin: "20 auto" }
+      }
+    >
+      <Avatar
+        alt={review.name}
+        src={review.avatar}
+        sx={{ width: 56, height: 56 }}
+        className="review-avatar"
+      />
+      <h3
+        className="review-name"
+        style={modalView ? { textAlign: "center" } : {}}
+      >
+        {review.name}
+      </h3>
+      <div style={modalView ? { textAlign: "center" } : {}}>
+        <Rating value={review.rating} readOnly className="review-rating" />
+      </div>
+      <p className="review-text">
+        {!modalView ? review.text.slice(0, 100) + "...." : review.text}
+      </p>
+    </div>
+  );
+};
+
+const Reviews = ({ guestsReviews }) => {
+  const [open, setOpen] = React.useState(false);
+  const [activeIndex, setActiveIndex] = React.useState(null);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  function handleClick(index) {
+    setActiveIndex(index);
+    handleOpen();
+  }
+
+  return (
+    <>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="gallery-modal"
+        aria-describedby="gallery-modal"
+      >
+        <Box sx={style}>
+          <ReviewsContent review={guestsReviews[activeIndex]} />
+        </Box>
+      </Modal>
+      <Slider {...settings} className="reviews">
+        {guestsReviews.map((review, index) => (
+          <div
+            key={index}
+            className="review-card"
+            onClick={() => handleClick(index)}
+          >
+            <ReviewsContent review={review} modalView={false} />
           </div>
-        </div>
-      ))}
-    </Slider>
+        ))}
+      </Slider>
+    </>
   );
 };
 
